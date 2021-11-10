be7c8a5a7b3f1ccc72b33622ee33866ff22a1296
pengluan <pengluan@tencent.com>
2021-11-10 10:54:45 +0800

    fix dockerfile

--- images/ubuntu-gpu/build.sh
+++ images/ubuntu-gpu/build.sh
@@ -1,32 +1,43 @@
-
 set -ex
 hubhost=ai.tencentmusic.com/tme-public
-base_image=nvidia/cuda:10.1-cudnn7-runtime-ubuntu18.04
+base_image=nvidia/cuda:10.1-cudnn7-devel-ubuntu18.04
 docker build -t $hubhost/ubuntu-gpu:cuda10.1-cudnn7 --build-arg FROM_IMAGES=$base_image -f cuda/Dockerfile .
-docker build -t $hubhost/ubuntu-gpu:cuda10.1-cudnn7-python3.6 --build-arg FROM_IMAGES=$hubhost/ubuntu-gpu:cuda10.1-cudnn7 --build-arg PYTHON_VERSION=python3.6  -f cuda/python/Dockerfile .
-docker build -t $hubhost/ubuntu-gpu:cuda10.1-cudnn7-python3.7 --build-arg FROM_IMAGES=$hubhost/ubuntu-gpu:cuda10.1-cudnn7 --build-arg PYTHON_VERSION=python3.7  -f cuda/python/Dockerfile .
+docker build -t $hubhost/ubuntu-gpu:cuda10.1-cudnn7-python3.6 --build-arg FROM_IMAGES=$hubhost/ubuntu-gpu:cuda10.1-cudnn7 --build-arg PYTHON_VERSION=python3.6 -f cuda/python/Dockerfile .
+docker build -t $hubhost/ubuntu-gpu:cuda10.1-cudnn7-python3.7 --build-arg FROM_IMAGES=$hubhost/ubuntu-gpu:cuda10.1-cudnn7 --build-arg PYTHON_VERSION=python3.7 -f cuda/python/Dockerfile .
 docker build -t $hubhost/ubuntu-gpu:cuda10.1-cudnn7-python3.8 --build-arg FROM_IMAGES=$hubhost/ubuntu-gpu:cuda10.1-cudnn7 --build-arg PYTHON_VERSION=python3.8 -f cuda/python/Dockerfile .
+docker push $hubhost/ubuntu-gpu:cuda10.1-cudnn7
+docker push $hubhost/ubuntu-gpu:cuda10.1-cudnn7-python3.6
+docker push $hubhost/ubuntu-gpu:cuda10.1-cudnn7-python3.7
+docker push $hubhost/ubuntu-gpu:cuda10.1-cudnn7-python3.8
 
 
-base_image=nvidia/cuda:10.0-cudnn7-runtime-ubuntu18.04
+base_image=nvidia/cuda:10.0-cudnn7-devel-ubuntu18.04
 docker build -t $hubhost/ubuntu-gpu:cuda10.0-cudnn7 --build-arg FROM_IMAGES=$base_image -f cuda/Dockerfile .
-docker build -t $hubhost/ubuntu-gpu:cuda10.0-cudnn7-python3.6 --build-arg FROM_IMAGES=$hubhost/ubuntu-gpu:cuda10.0-cudnn7 --build-arg PYTHON_VERSION=python3.6  -f cuda/python/Dockerfile .
-docker build -t $hubhost/ubuntu-gpu:cuda10.0-cudnn7-python3.7 --build-arg FROM_IMAGES=$hubhost/ubuntu-gpu:cuda10.0-cudnn7 --build-arg PYTHON_VERSION=python3.7  -f cuda/python/Dockerfile .
+docker build -t $hubhost/ubuntu-gpu:cuda10.0-cudnn7-python3.6 --build-arg FROM_IMAGES=$hubhost/ubuntu-gpu:cuda10.0-cudnn7 --build-arg PYTHON_VERSION=python3.6 -f cuda/python/Dockerfile .
+docker build -t $hubhost/ubuntu-gpu:cuda10.0-cudnn7-python3.7 --build-arg FROM_IMAGES=$hubhost/ubuntu-gpu:cuda10.0-cudnn7 --build-arg PYTHON_VERSION=python3.7 -f cuda/python/Dockerfile .
 docker build -t $hubhost/ubuntu-gpu:cuda10.0-cudnn7-python3.8 --build-arg FROM_IMAGES=$hubhost/ubuntu-gpu:cuda10.0-cudnn7 --build-arg PYTHON_VERSION=python3.8 -f cuda/python/Dockerfile .
+docker push $hubhost/ubuntu-gpu:cuda10.0-cudnn7
+docker push $hubhost/ubuntu-gpu:cuda10.0-cudnn7-python3.6
+docker push $hubhost/ubuntu-gpu:cuda10.0-cudnn7-python3.7
+docker push $hubhost/ubuntu-gpu:cuda10.0-cudnn7-python3.8
 
-
-base_image=nvidia/cuda:9.1-cudnn7-runtime-ubuntu16.04
+base_image=nvidia/cuda:9.1-cudnn7-devel-ubuntu16.04
 docker build -t $hubhost/ubuntu-gpu:cuda9.1-cudnn7 --build-arg FROM_IMAGES=$base_image -f cuda/Dockerfile .
-docker build -t $hubhost/ubuntu-gpu:cuda9.1-cudnn7-python3.6 --build-arg FROM_IMAGES=$hubhost/ubuntu-gpu:cuda9.1-cudnn7 --build-arg PYTHON_VERSION=python3.6  -f cuda/python/Dockerfile .
-docker build -t $hubhost/ubuntu-gpu:cuda9.1-cudnn7-python3.7 --build-arg FROM_IMAGES=$hubhost/ubuntu-gpu:cuda9.1-cudnn7 --build-arg PYTHON_VERSION=python3.7  -f cuda/python/Dockerfile .
+docker build -t $hubhost/ubuntu-gpu:cuda9.1-cudnn7-python3.6 --build-arg FROM_IMAGES=$hubhost/ubuntu-gpu:cuda9.1-cudnn7 --build-arg PYTHON_VERSION=python3.6 -f cuda/python/Dockerfile .
+docker build -t $hubhost/ubuntu-gpu:cuda9.1-cudnn7-python3.7 --build-arg FROM_IMAGES=$hubhost/ubuntu-gpu:cuda9.1-cudnn7 --build-arg PYTHON_VERSION=python3.7 -f cuda/python/Dockerfile .
 docker build -t $hubhost/ubuntu-gpu:cuda9.1-cudnn7-python3.8 --build-arg FROM_IMAGES=$hubhost/ubuntu-gpu:cuda9.1-cudnn7 --build-arg PYTHON_VERSION=python3.8 -f cuda/python/Dockerfile .
+docker push $hubhost/ubuntu-gpu:cuda9.1-cudnn7
+docker push $hubhost/ubuntu-gpu:cuda9.1-cudnn7-python3.6
+docker push $hubhost/ubuntu-gpu:cuda9.1-cudnn7-python3.7
+docker push $hubhost/ubuntu-gpu:cuda9.1-cudnn7-python3.8
 
-
-base_image=nvidia/cuda:9.0-cudnn7-runtime-ubuntu16.04
+base_image=nvidia/cuda:9.0-cudnn7-devel-ubuntu16.04
 docker build -t $hubhost/ubuntu-gpu:cuda9.0-cudnn7 --build-arg FROM_IMAGES=$base_image -f cuda/Dockerfile .
-docker build -t $hubhost/ubuntu-gpu:cuda9.0-cudnn7-python3.6 --build-arg FROM_IMAGES=$hubhost/ubuntu-gpu:cuda9.0-cudnn7 --build-arg PYTHON_VERSION=python3.6  -f cuda/python/Dockerfile .
-docker build -t $hubhost/ubuntu-gpu:cuda9.0-cudnn7-python3.7 --build-arg FROM_IMAGES=$hubhost/ubuntu-gpu:cuda9.0-cudnn7 --build-arg PYTHON_VERSION=python3.7  -f cuda/python/Dockerfile .
+docker build -t $hubhost/ubuntu-gpu:cuda9.0-cudnn7-python3.6 --build-arg FROM_IMAGES=$hubhost/ubuntu-gpu:cuda9.0-cudnn7 --build-arg PYTHON_VERSION=python3.6 -f cuda/python/Dockerfile .
+docker build -t $hubhost/ubuntu-gpu:cuda9.0-cudnn7-python3.7 --build-arg FROM_IMAGES=$hubhost/ubuntu-gpu:cuda9.0-cudnn7 --build-arg PYTHON_VERSION=python3.7 -f cuda/python/Dockerfile .
 docker build -t $hubhost/ubuntu-gpu:cuda9.0-cudnn7-python3.8 --build-arg FROM_IMAGES=$hubhost/ubuntu-gpu:cuda9.0-cudnn7 --build-arg PYTHON_VERSION=python3.8 -f cuda/python/Dockerfile .
-
-
+docker push $hubhost/ubuntu-gpu:cuda9.0-cudnn7
+docker push $hubhost/ubuntu-gpu:cuda9.0-cudnn7-python3.6
+docker push $hubhost/ubuntu-gpu:cuda9.0-cudnn7-python3.7
+docker push $hubhost/ubuntu-gpu:cuda9.0-cudnn7-python3.8
 
