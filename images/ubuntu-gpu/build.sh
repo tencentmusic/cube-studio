a2aef71c1c4eec4f2470205af5c70448fed7c228
pengluan <pengluan@tencent.com>
2022-04-26 11:58:02 +0800

    add gpu docker

--- images/ubuntu-gpu/build.sh
+++ images/ubuntu-gpu/build.sh
@@ -1,5 +1,17 @@
 set -ex
 hubhost=ai.tencentmusic.com/tme-public
+
+base_image=nvidia/cuda:10.2-cudnn7-devel-ubuntu18.04
+docker build -t $hubhost/ubuntu-gpu:cuda10.2-cudnn7 --build-arg FROM_IMAGES=$base_image -f cuda/Dockerfile .
+docker build -t $hubhost/ubuntu-gpu:cuda10.2-cudnn7-python3.6 --build-arg FROM_IMAGES=$hubhost/ubuntu-gpu:cuda10.2-cudnn7 --build-arg PYTHON_VERSION=python3.6 -f cuda/python/Dockerfile .
+docker build -t $hubhost/ubuntu-gpu:cuda10.2-cudnn7-python3.7 --build-arg FROM_IMAGES=$hubhost/ubuntu-gpu:cuda10.2-cudnn7 --build-arg PYTHON_VERSION=python3.7 -f cuda/python/Dockerfile .
+docker build -t $hubhost/ubuntu-gpu:cuda10.2-cudnn7-python3.8 --build-arg FROM_IMAGES=$hubhost/ubuntu-gpu:cuda10.2-cudnn7 --build-arg PYTHON_VERSION=python3.8 -f cuda/python/Dockerfile .
+docker push $hubhost/ubuntu-gpu:cuda10.2-cudnn7
+docker push $hubhost/ubuntu-gpu:cuda10.2-cudnn7-python3.6
+docker push $hubhost/ubuntu-gpu:cuda10.2-cudnn7-python3.7
+docker push $hubhost/ubuntu-gpu:cuda10.2-cudnn7-python3.8
+
+
 base_image=nvidia/cuda:10.1-cudnn7-devel-ubuntu18.04
 docker build -t $hubhost/ubuntu-gpu:cuda10.1-cudnn7 --build-arg FROM_IMAGES=$base_image -f cuda/Dockerfile .
 docker build -t $hubhost/ubuntu-gpu:cuda10.1-cudnn7-python3.6 --build-arg FROM_IMAGES=$hubhost/ubuntu-gpu:cuda10.1-cudnn7 --build-arg PYTHON_VERSION=python3.6 -f cuda/python/Dockerfile .
