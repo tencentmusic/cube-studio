0f5570ecea47a0eb14d8609740918bd3ad754f4e
xiaoyangmai <>
2022-05-19 20:21:10 +0800

    dev

--- install/kubernetes/tmeps/build.sh
+++ install/kubernetes/tmeps/build.sh
@@ -1,9 +1,6 @@
 your_docker_registry=mirrors.tencent.com/uthermai
 
-# 用于训练过程中，ps、worker等节点
-docker build --network=host -t ${your_docker_registry}/tfra-on-cube:trainning -f Dockerfile_tfra_trainning .
-
-# 用于推理服务
-docker build --network=host -t ${your_docker_registry}/tfra-on-cube:serving -f Dockerfile_tfra_serving .
+docker build --network=host -t ${your_docker_registry}/tmeps:trainning -f Dockerfile_tfra_trainning . && /
+docker build --network=host -t ${your_docker_registry}/tmeps:serving -f Dockerfile_tfra_serving 
 
 #docker build --network=host -t ${your_docker_registry}/tfra-on-cube:serving --build-arg TF_SERVING_VERSION_GIT_COMMIT=2.5.2 -f Dockerfile_tfra_serving .
---
+++ install/kubernetes/tmeps/deploy.sh
@@ -0,0 +1,1 @@
+kubectl get nodes
