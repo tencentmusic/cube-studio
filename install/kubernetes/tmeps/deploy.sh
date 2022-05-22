539505c64e43be3e58b2e7ad2ecda1cf4c2a2a22
xiaoyangmai <916955404@qq.com.com>
2022-05-23 01:46:59 +0800

    dev

--- install/kubernetes/tmeps/deploy.sh
+++ install/kubernetes/tmeps/deploy.sh
@@ -1,1 +1,20 @@
-kubectl --kubeconfig ~/job-template/job/pkgs/config/tke-kubeconfig get nodes
+kubectl --kubeconfig /mnt/uthermai/job-template/job/pkgs/config/tke-kubeconfig apply -f resources/ns.yaml
+sleep 2
+
+kubectl --kubeconfig /mnt/uthermai/job-template/job/pkgs/config/tke-kubeconfig apply -f conf/
+kubectl --kubeconfig /mnt/uthermai/job-template/job/pkgs/config/tke-kubeconfig apply -f resources/redis_standlone.yaml
+#kubectl --kubeconfig /mnt/uthermai/job-template/job/pkgs/config/tke-kubeconfig wait -f resources/redis_standlone.yaml --for condition=available
+sleep 2
+
+if [ -z $(which kustomize) ];then
+        echo  "kustomize 未安装"
+	curl -Lo ./kustomize https://github.com/kubernetes-sigs/kustomize/releases/download/v3.2.0/kustomize_3.2.0_linux_amd64
+	chmod 777 ./kustomize
+	mv ./kustomize /usr/local/bin/
+else
+        echo "kustomize 已安装"
+fi
+
+kustomize build resources | kubectl apply -f -
+#kustomize build resources | kubectl delete -f -
+
