32130aad98a8c3078d34842dd204ad39ed5c1445
xiaoyangmai <>
2022-05-25 16:49:16 +0800

    dev

--- install/kubernetes/tmeps/build.sh
+++ install/kubernetes/tmeps/build.sh
@@ -1,8 +1,7 @@
 
-your_docker_registry=ai.tencentmusic.com/tme-public
-
-docker build --network=host -t ${your_docker_registry}/tmeps:trainning -f Dockerfile_tfra_trainning . && /
-docker build --network=host -t ${your_docker_registry}/tmeps:serving -f Dockerfile_tfra_serving .
-
+docker build --network=host -t ai.tencentmusic.com/tme-public/tmeps:trainning -f Dockerfile_tfra_trainning . && /
+docker build --network=host -t ai.tencentmusic.com/tme-public/tmeps:serving -f Dockerfile_tfra_serving .
+docker push ai.tencentmusic.com/tme-public/tmeps:trainning
+docker push ai.tencentmusic.com/tme-public/tmeps:serving
 
 #docker build --network=host -t ${your_docker_registry}/tfra-on-cube:serving --build-arg TF_SERVING_VERSION_GIT_COMMIT=2.5.2 -f Dockerfile_tfra_serving .
