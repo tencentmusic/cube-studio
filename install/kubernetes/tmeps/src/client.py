9a5222550e9758ea58502d81f4a5b56dde23dcc0
pengluan <pengluan@tencent.com>
2022-05-25 15:31:11 +0800

    update tmeps

--- install/kubernetes/tmeps/src/client.py
+++ install/kubernetes/tmeps/src/client.py
@@ -1,1 +1,1 @@
-curl -d '{"instances": [{"movie_id":1, "user_id":1, "user_rating":0.222}]}' -XPOST http://172.21.2.219:8501/v1/models/test/versions/1640092867:predict
+curl -d '{"instances": [{"movie_id":1, "user_id":1, "user_rating":0.222}]}' -XPOST http://test-model-server-svc.tfra-dev:8501/v1/models/test/versions/1640092867:predict
