02aeff7523efbb5b813b53d3e176343a8762a01d
pengluan <pengluan@tencent.com>
2022-06-28 19:52:29 +0800

    添加内网部署镜像打包

--- images/serving/tfserving/build.sh
+++ images/serving/tfserving/build.sh
@@ -1,13 +1,13 @@
 set -ex
 hubhost=ccr.ccs.tencentyun.com/cube-studio
 
-arr=("1.11.0" "1.11.0-gpu" "1.12.0" "1.12.0-gpu" "1.13.0" "1.13.0-gpu" "1.14.0" "1.14.0-gpu" "2.0.0" "2.0.0-gpu" "2.1.4" "2.1.4-gpu" "2.2.3" "2.2.3-gpu" "2.3.4" "2.3.4-gpu" "2.4.3" "2.4.3-gpu" "2.5.2" "2.5.2-gpu" "2.6.0" "2.6.0-gpu")
+arr=("1.14.0" "1.14.0-gpu" "2.0.0" "2.0.0-gpu" "2.1.4" "2.1.4-gpu" "2.2.3" "2.2.3-gpu" "2.3.4" "2.3.4-gpu" "2.4.3" "2.4.3-gpu" "2.5.2" "2.5.2-gpu" "2.6.0" "2.6.0-gpu")
 
 for value in ${arr[@]}
 do
     echo $value
     docker build -t $hubhost/tfserving:$value --build-arg FROM_IMAGES=tensorflow/serving:$value .
-    docker push $hubhost/$value
+    docker push $hubhost/tfserving:$value
 done
 
 
