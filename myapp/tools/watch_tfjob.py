04fcae7013fccc7e3b887c6b50405863273e1567
pengluan <pengluan@tencent.com>
2022-07-24 17:46:23 +0800

    修复监听问题

--- myapp/tools/watch_tfjob.py
+++ myapp/tools/watch_tfjob.py
@@ -2,8 +2,7 @@
 
 import time,datetime,logging,os,sys
 import asyncio
-from kubernetes import client as k8s_client
-from kubernetes import config as k8s_config
+from kubernetes import client
 from kubernetes import watch
 from os import path
 import json
@@ -37,8 +36,7 @@ else:
     clusters = conf.get('CLUSTERS',{})
     if clusters and cluster in clusters:
         kubeconfig = clusters[cluster].get('KUBECONFIG','')
-        k8s_client = K8s(kubeconfig)
-        # k8s_config.kube_config.load_kube_config(config_file=kubeconfig)
+        K8s(kubeconfig)
     else:
         print('no kubeconfig in cluster %s' % cluster)
         exit(1)
@@ -242,8 +240,7 @@ def save_history(tfjob,dbsession):
 
 # @pysnooper.snoop()
 def check_crd_exist(group,version,namespace,plural,name):
-    client = k8s_client.CustomObjectsApi()
-    exist_crd = client.get_namespaced_custom_object(group,version,namespace,plural,name)
+    exist_crd = client.CustomObjectsApi().get_namespaced_custom_object(group,version,namespace,plural,name)
     return exist_crd
 
 
@@ -304,7 +301,7 @@ def listen_crd():
     print('begin listen')
     while(True):
         try:
-            for event in w.stream(k8s_client.CustomObjectsApi().list_namespaced_custom_object, group=crd_info['group'],
+            for event in w.stream(client.CustomObjectsApi().list_namespaced_custom_object, group=crd_info['group'],
                                   version=crd_info['version'],
                                   namespace=namespace, plural=crd_info['plural'], pretty='true'):
 
