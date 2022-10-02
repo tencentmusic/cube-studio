228c7e42a183fbe4a9a9074d2925f37668966389
cdllp2 <luanpengcdlllp2@163.com>
2022-10-02 11:15:23 +0800

    add aihub

--- job-template/service/ai-photo/server-web.py
+++ job-template/service/ai-photo/server-web.py
@@ -46,6 +46,8 @@ def classify_rest():
     try:
         data = request.json
         txt = data.get('txt','姓')
+        if not txt:
+            txt='姓'
         image_decode = base64.b64decode(data['image_data'])
         image_path = os.path.join(UPLOAD_FOLDER, str(datetime.datetime.now()) + ".jpg")
         nparr = np.fromstring(image_decode, np.uint8)
