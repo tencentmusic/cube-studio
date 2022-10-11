940b5b041acb035e7479a4c1416a01415f1bc3f1
cdllp2 <luanpengcdlllp2@163.com>
2022-10-11 10:31:35 +0800

    add bidata jupyter

--- images/jupyter-notebook/bigdata/build.sh
+++ images/jupyter-notebook/bigdata/build.sh
@@ -2,5 +2,5 @@ set -ex
 hubhost=ccr.ccs.tencentyun.com/cube-studio
 
 # 构建bigdata镜像
-docker build -t  $hubhost/notebook:jupyter-ubuntu-cpu-bigdata -f Dockerfile .
-docker push $hubhost/notebook:jupyter-ubuntu-cpu-bigdata
+docker build -t  $hubhost/notebook:jupyter-ubuntu-bigdata -f Dockerfile .
+docker push $hubhost/notebook:jupyter-ubuntu-bigdata
