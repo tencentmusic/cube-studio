15c1aa8bde5e2ce84094596ef75144a83e0b8c4d
cdllp2 <luanpengcdlllp2@163.com>
2022-10-13 11:12:29 +0800

    add ssh

--- images/jupyter-notebook/machinelearning/init.sh
+++ images/jupyter-notebook/machinelearning/init.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
+# 配置ssh链接
+echo "Port ${SSH_PORT}" >> /etc/ssh/sshd_config
+sed -i "s/#PermitEmptyPasswords no/PermitEmptyPasswords yes/g" /etc/ssh/sshd_config
+sed -i "s/#PermitRootLogin yes/PermitRootLogin yes/g" /etc/ssh/sshd_config
+sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh/sshd_config
+echo root:cube-studio | chpasswd
+service ssh restart
+# 客户端连接命令，    ssh -p ${SSH_PORT} root@${SERVICE_EXTERNAL_IP}
+
+sed -i "s/localhost/${SERVICE_EXTERNAL_IP}/g" /examples/ssh连接
+sed -i "s/localport/${SSH_PORT}/g" /examples/ssh连接
+
+# 配置example
 ln -s /examples /mnt/${USERNAME}/
+
 
