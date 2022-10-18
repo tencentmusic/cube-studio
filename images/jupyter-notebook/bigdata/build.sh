cce04b8c69d2721f6b6c2b1cba21ad10f5ed96ce
jacktao007 <jacktao006@gmail.com>
2022-10-18 12:24:24 -0400

    feat(jupyter-notebook/bigdata):parameterize dockerfile and add mirrors params

--- images/jupyter-notebook/bigdata/build.sh
+++ images/jupyter-notebook/bigdata/build.sh
@@ -2,5 +2,6 @@ set -ex
 hubhost=ccr.ccs.tencentyun.com/cube-studio
 
 # 构建bigdata镜像
-docker build -t  $hubhost/notebook:jupyter-ubuntu-bigdata -f Dockerfile .
-docker push $hubhost/notebook:jupyter-ubuntu-bigdata
+#docker build -t  $hubhost/notebook:jupyter-ubuntu-bigdata -f Dockerfile .
+docker build --build-arg APACHE_MIRROR=https://mirrors.aliyun.com/apache --build-arg  PIPI_MIRROR_ENABLE=true --build-arg  UBUNTU_MIRROR_ENABLE=true -t  $hubhost/notebook:jupyter-ubuntu-bigdata1 -f Dockerfile .
+#docker push $hubhost/notebook:jupyter-ubuntu-bigdata
