4c5411f546090b241c19a0bf7dd30bd0ed6e30f8
cdllp2 <luanpengcdlllp2@163.com>
2022-10-31 15:15:33 +0800

    add sdk

--- sdk/src/cubestudio/aihub/docker/build.sh
+++ sdk/src/cubestudio/aihub/docker/build.sh
@@ -6,3 +6,8 @@ docker build -t ccr.ccs.tencentyun.com/cube-studio/aihub:base-python3.6  -f Dock
 docker push ccr.ccs.tencentyun.com/cube-studio/aihub:base-python3.6
 docker build -t ccr.ccs.tencentyun.com/cube-studio/aihub:base-cuda11.4-python3.9  -f Dockerfile-cuda11.4-python3.9 .
 docker push ccr.ccs.tencentyun.com/cube-studio/aihub:base-cuda11.4-python3.9
+
+docker build -t ccr.ccs.tencentyun.com/cube-studio/aihub:base-cuda11.4  -f Dockerfile-cuda11.4 .
+docker push ccr.ccs.tencentyun.com/cube-studio/aihub:base-cuda11.4
+docker build -t ccr.ccs.tencentyun.com/cube-studio/aihub:base  -f Dockerfile.
+docker push ccr.ccs.tencentyun.com/cube-studio/aihub:base
