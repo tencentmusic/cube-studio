97ddb1c10c49a12b686f6bc19c4e87c15f2ee88d
cdllp2 <luanpengcdlllp2@163.com>
2022-11-04 16:47:53 +0800

    fix

--- sdk/example/animegan/init.sh
+++ sdk/example/animegan/init.sh
@@ -3,6 +3,6 @@ pip install pyarmor pycryptodome openvino scipy tqdm pyyaml yapf opencv-python n
 
 apt install -y g++-8-i686-linux-gnu gcc-8-i686-linux-gnu gcc-i686-linux-gnu g++-i686-linux-gnu ffmpeg libsm6 libxext6
 mkdir -p github
-wget https://huggingface.co/spaces/TachibanaYoshino/AnimeGANv3/resolve/main/AnimeGANv3_bin.so -P /gihutb
-wget https://huggingface.co/spaces/TachibanaYoshino/AnimeGANv3/resolve/main/AnimeGANv3_src.so -P /gihutb
+wget https://huggingface.co/spaces/TachibanaYoshino/AnimeGANv3/resolve/main/AnimeGANv3_bin.so -P /github
+wget https://huggingface.co/spaces/TachibanaYoshino/AnimeGANv3/resolve/main/AnimeGANv3_src.so -P /github
 
