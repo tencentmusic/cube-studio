19908c8d8d135781f488e3d9a72ae01f1b0ebc6e
JLWL <1174909880@qq.com>
2022-11-19 15:26:05 +0800

    Update init.sh

--- aihub/deep-learning/yolov5-6.1/init.sh
+++ aihub/deep-learning/yolov5-6.1/init.sh
@@ -1,5 +1,5 @@
 apt install zip
-wget
-unzip
-rm yolov5.zip
 pip install matplotlib numpy opencv-python Pillow PyYAML requests scipy torch torchvision tqdm tensorboard pandas seaborn
+#wget 源代码文件地址
+#unzip 源代码文件
+#rm 源代码文件
