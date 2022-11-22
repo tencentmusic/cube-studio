b0f5729243f428f884397ac200879dd99939da5a
cdllp2 <luanpengcdlllp2@163.com>
2022-11-22 23:10:39 +0800

    update docker image tag

--- aihub/deep-learning/paddlespeech-cls/app.py
+++ aihub/deep-learning/paddlespeech-cls/app.py
@@ -17,7 +17,7 @@ class Speech_Cls_Model(Model):
     status = 'online'
     version = 'v20221114'
     doc = 'https://github.com/PaddlePaddle/PaddleSpeech'  # 'https://帮助文档的链接地址'
-    # pic = 'https://images.nightcafe.studio//assets/stable-tile.jpg'  # https://应用描述的缩略图/可以直接使用应用内的图片文件地址
+    pic = 'example.jpg'  # https://应用描述的缩略图/可以直接使用应用内的图片文件地址
 
     # inference_resource = {
     #     "resource_gpu": "1"
