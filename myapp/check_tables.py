7c80213f0b69a226fd16e17fa66c16440f7e5d7b
cdllp2 <luanpengcdlllp2@163.com>
2022-11-23 16:54:19 +0800

    add favorite model row

--- myapp/check_tables.py
+++ myapp/check_tables.py
@@ -19,7 +19,7 @@ def check_tables():
         results = list(cursor.fetchall())
         results = [item[0] for item in results]
         print(results)
-        for table_name in ['ab_permission','ab_permission_view','ab_permission_view_role','ab_register_user','ab_role','ab_user','ab_user_role','ab_view_menu','alembic_version','dimension','docker','images','inferenceservice','job_template','logs','metadata_metric','model','nni','notebook','pipeline','project','project_user','pytorchjob','repository','run','service','service_pipeline','task','metadata_table','tfjob','user_attribute','workflow','xgbjob']:
+        for table_name in ['ab_permission','ab_permission_view','ab_permission_view_role','ab_register_user','ab_role','ab_user','ab_user_role','ab_view_menu','alembic_version','dimension','docker','images','inferenceservice','job_template','logs','metadata_metric','model','nni','notebook','pipeline','project','project_user','pytorchjob','repository','run','service','service_pipeline','task','metadata_table','tfjob','workflow','xgbjob']:
             if table_name not in results:
                 print('kubeflow db下，table %s不完整，请\n1、mysql: drop dabatase kubeflow\n2、重启当前pod'%table_name)
                 exit(1)
