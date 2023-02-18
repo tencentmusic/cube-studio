9c1aa5862c8f4d54027164d15f1cdc9f8041b36b
pengluan <pengluan@tencent.com>
2023-02-18 15:20:47 +0800

    fix

--- myapp/models/model_aihub.py
+++ myapp/models/model_aihub.py
@@ -50,13 +50,15 @@ class Aihub(Model,MyappModelBase):
         train_url = "/model_market/all/api/train/" + self.uuid if self.status == 'online' and (self.job_template or self.pipeline) else ""
         # if not job_template:
         #     train_url=disable
-        service_url = "/model_market/all/api/service/" + self.uuid if self.status == 'online' and (self.service or self.inference) else ""
-        service_text='部署web'
+        offline_inference_url = "/model_market/all/api/inference/offline/" + self.uuid
+        offline_inference_text = '推理'
+        web_url = "/model_market/all/api/service/" + self.uuid if self.status == 'online' and (self.service or self.inference) else ""
+        web_text='部署web'
         link = 'https://github.com/tencentmusic/cube-studio/tree/master/aihub/deep-learning/' + self.name
         expand = json.loads(self.expand) if self.expand else {}
         if expand.get('status','offline')=='online':
-            service_url = "/model_market/all/api/service/delete/" + self.uuid if self.status == 'online' and (self.service or self.inference) else ""
-            service_text = '卸载web'
+            web_url = "/model_market/all/api/service/delete/" + self.uuid if self.status == 'online' and (self.service or self.inference) else ""
+            web_text = '卸载web'
             link = f'/aihub/{self.name}/'
 
         pic_url = '/static/aihub/deep-learning/'+self.name+'/'+self.pic
@@ -79,7 +81,7 @@ class Aihub(Model,MyappModelBase):
         <div style="border-top: 1px solid rgba(0,0,0,.06);" class="ptb8 d-f ac jc-b">
             <a class="flex1 ta-c" target=_blank style="border-right: 1px solid rgba(0,0,0,.06);" href='{notebook_url}'>开发</a>
             <a class="flex1 ta-c" target=_blank style="{"color:Gray;" if train_url==disable else ""}border-right: 1px solid rgba(0,0,0,.06);" href="{train_url}">训练</a>
-            <a class="flex1 ta-c" target=_blank style="border-right: 1px solid rgba(0,0,0,.06);" href='{service_url}'>{service_text}</a>
+            <a class="flex1 ta-c" target=_blank style="border-right: 1px solid rgba(0,0,0,.06);" href='{web_url}'>{web_text}</a>
         </div>
     </div>
 </div>
