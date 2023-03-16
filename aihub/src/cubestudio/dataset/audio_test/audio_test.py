0345cf77350092c39a612255fce9f22f3fb2ac3d
pengluan <pengluan@tencent.com>
2023-03-16 14:50:03 +0800

    添加gpt3训练

--- aihub/src/cubestudio/dataset/audio_test/audio_test.py
+++
@@ -1,9 +0,0 @@
-
-from pyarrow import csv
-import os,sys
-import pysnooper
-
-# @pysnooper.snoop()
-def load(dataset_name):
-    csv_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),f'{dataset_name}.csv')
-    return csv.read_csv(csv_path)
