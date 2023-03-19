0dcd5dd9108d4e13fd0cb17018248093a4aa73c0
root <825485697@qq.com>
2023-03-19 16:12:42 +0800

    add speech

---
+++ aihub/modelscope/speech-inverse-text-processing-fun-text-processing-itn-vi/download_model.py
@@ -0,0 +1,6 @@
+
+
+from modelscope.pipelines import pipeline
+from modelscope.utils.constant import Tasks
+
+p = pipeline('inverse-text-processing', 'damo/speech_inverse_text_processing_fun-text-processing-itn-vi')
