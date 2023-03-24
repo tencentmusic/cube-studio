f4cc6eabb9fdea7bc7649212d7bd92e61f9ebb39
pengluan <pengluan@tencent.com>
2023-03-24 14:48:58 +0800

    添加dataset模板

--- aihub/src/docker/entrypoint.sh
+++ aihub/src/docker/entrypoint.sh
@@ -5,6 +5,7 @@ pip install pysnooper requests Flask==2.1.3 kubernetes celery redis cryptography
 
 cp /src/docker/nginx.conf /etc/nginx/nginx.conf
 cp /src/docker/default.conf /etc/nginx/conf.d/default.conf
+cp /src/docker/restart.conf /etc/systemd/system/nginx.service.d/
 service nginx stop
 nginx -g "daemon off;" &
 echo "started nginx"
