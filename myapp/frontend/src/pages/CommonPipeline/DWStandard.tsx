14970796aa6b593176b26047d7c7cecb5534e9ce
flytengzeng <flytengzeng@tencent.com>
2023-03-27 10:36:39 +0800

    feature:通用血缘系统

---
+++ myapp/frontend/src/pages/CommonPipeline/DWStandard.tsx
@@ -0,0 +1,9 @@
+import CommonPipeline from './CommonPipeline'
+
+export default function DWStandard() {
+    return (
+        <div className="fade-in h100 d-f fd-c">
+            <CommonPipeline />
+        </div>
+    )
+}
