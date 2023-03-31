45692a36b8685a55207e00b3e09026bc2b40c02f
pengluan <pengluan@tencent.com>
2023-03-31 13:37:35 +0800

    添加dataset模板

--- aihub/modelscope/speech-transformer-lm-zh-cn-common-vocab8404-pytorch/download_model.py
+++
@@ -1,6 +0,0 @@
-
-
-from modelscope.pipelines import pipeline
-from modelscope.utils.constant import Tasks
-
-p = pipeline('language-score-prediction', 'damo/speech_transformer_lm_zh-cn-common-vocab8404-pytorch')
