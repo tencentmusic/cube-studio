c7095763eaae272505a74ff7a7622ea7af4f4a6f
chendile <pengluan@tencent.com>
2023-04-06 22:54:23 +0800

    reduce images and donwload parallel

--- install/kubernetes/rancher/all_image.py
+++ install/kubernetes/rancher/all_image.py
@@ -180,16 +180,13 @@ for image in images:
     image_name = HOST + image.replace(HOST,'').replace('/', '-').replace('@sha256', '')
 
     # 可联网机器上拉取公有镜像并推送到私有仓库
-    # print('docker pull %s' % image)
-    # print('docker tag %s %s' % (image, image_name))
-    # print('docker push %s' % (image_name))
+    # print('docker pull %s && docker tag %s %s && docker push %s &' % (image,image,image_name,image_name))
 
-    # # # # 内网机器上拉取私有仓库镜像
+    # # # 内网机器上拉取私有仓库镜像
     # image=image.replace('@sha256','')
     # print("docker pull %s && docker tag %s %s &" % (image_name,image_name,image))
-    # # print("docker tag %s %s"%(image_name,image))
 
-    # # # 拉取公有镜像
+    # # 拉取公有镜像
     image=image.replace('@sha256','')
     print("docker pull %s &" % (image,))
 
