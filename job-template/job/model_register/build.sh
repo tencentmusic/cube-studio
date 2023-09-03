906feb246130946be2b260a319b311d5455c706e
chendile <cdllp1993@sina.com>
2023-09-03 18:21:29 +0800

    更新模型注册模板

--- job-template/job/model_register/build.sh
+++ job-template/job/model_register/build.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-docker build -t ccr.ccs.tencentyun.com/cube-studio/model_register:20221001 -f job/model_register/Dockerfile .
-docker push ccr.ccs.tencentyun.com/cube-studio/model_register:20221001
+docker build -t ccr.ccs.tencentyun.com/cube-studio/model_register:20230501 -f job/model_register/Dockerfile .
+docker push ccr.ccs.tencentyun.com/cube-studio/model_register:20230501
 
 
 
