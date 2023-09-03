244488fc097bfee457b0641db73f3ab72b82a805
chendile <cdllp1993@sina.com>
2023-09-03 18:20:29 +0800

    更新数据集导入模板

--- job-template/job/dataset/build.sh
+++ job-template/job/dataset/build.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-docker build -t ccr.ccs.tencentyun.com/cube-studio/dataset:20230401 -f job/dataset/Dockerfile .
-docker push ccr.ccs.tencentyun.com/cube-studio/dataset:20230401
+docker build -t ccr.ccs.tencentyun.com/cube-studio/dataset:20230501 -f job/dataset/Dockerfile .
+docker push ccr.ccs.tencentyun.com/cube-studio/dataset:20230501
 
-
+#docker buildx build --platform linux/amd64,linux/arm64 -t ccr.ccs.tencentyun.com/cube-studio/dataset:20230501 -f job/dataset/Dockerfile . --push
 
