544cab82f00a9c0a228d5832c0ddc2a82eb6d961
data-infra <825485697@qq.com>
2024-02-04 14:46:26 +0800

    修复前端部分bug

--- install/docker/entrypoint.sh
+++ install/docker/entrypoint.sh
@@ -26,7 +26,7 @@ if [ "$STAGE" = "build" ]; then
   # 构建前端主体
   cd /home/myapp/myapp/frontend && npm install && npm run build
   # 构建机器学习pipeline
-  cd /home/myapp/myapp/vision && yarn && npm run build
+  cd /home/myapp/myapp/vision && npm install && npm run build
   # 构建数据ETL pipeline
   cd /home/myapp/myapp/visionPlus && yarn && npm run build
 elif [ "$STAGE" = "dev" ]; then
