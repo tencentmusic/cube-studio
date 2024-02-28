83fe4c77c47891368f31fd63ca10babcb61d0741
data-infra <825485697@qq.com>
2024-02-28 14:51:26 +0800

    更新推理服务镜像版本

--- images/serving/torchserver/build.sh
+++ images/serving/torchserver/build.sh
@@ -1,7 +1,8 @@
 #set -ex
 hubhost=ccr.ccs.tencentyun.com/cube-studio
 
-arr=("torchserve:0.4.2-cpu" "torchserve:0.4.2-gpu" "torchserve:0.5.3-cpu" "torchserve:0.5.3-gpu" "torchserve:0.6.0-cpu" "torchserve:0.6.0-gpu")
+#arr=("torchserve:0.4.2-cpu" "torchserve:0.4.2-gpu" "torchserve:0.5.3-cpu" "torchserve:0.5.3-gpu" "torchserve:0.6.0-cpu" "torchserve:0.6.0-gpu")
+arr=("torchserve:0.7.1-cpu" "torchserve:0.7.1-gpu" "torchserve:0.8.2-cpu" "torchserve:0.8.2-gpu" "torchserve:0.9.0-cpu" "torchserve:0.9.0-gpu")
 
 for value in ${arr[@]}
 do
--- images/serving/triton-server/build.sh
+++ images/serving/triton-server/build.sh
@@ -1,7 +1,7 @@
 set -ex
 hubhost=ccr.ccs.tencentyun.com/cube-studio
 
-arr=("tritonserver:22.07-py3" "tritonserver:21.12-py3" "tritonserver:21.09-py3")
+arr=("tritonserver:24.01-py3" "tritonserver:23.12-py3" "tritonserver:22.12-py3" "tritonserver:21.12-py3" "tritonserver:20.12-py3")
 
 for value in ${arr[@]}
 do
