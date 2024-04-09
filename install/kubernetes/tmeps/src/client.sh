a588bf1a3793c4efd51a1bb50c9ff5d89e737273
xiaoman.gao@extremevision.com.cn <825485697@qq.com>
2024-04-09 22:03:31 +0800

    删除非必要代码

--- install/kubernetes/tmeps/src/client.sh
+++
@@ -1,2 +0,0 @@
-curl -XGET http://test-model-server-svc.tfra-dev:8501/v1/models/test_model
-curl -d '{"instances": [{"movie_id":1, "user_id":1, "user_rating":0.222}]}' -XPOST http://test-model-server-svc.tfra-dev:8501/v1/models/test/versions/1640092867:predict
