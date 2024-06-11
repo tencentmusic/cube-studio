ba369c50dc64eb45134d924494c41bc850569eaa
xiaoman.gao@extremevision.com.cn <825485697@qq.com>
2024-06-11 13:53:44 +0800

    增加新cuda版本的gpu镜像

--- images/ubuntu-gpu/build.sh
+++ images/ubuntu-gpu/build.sh
@@ -1,14 +1,23 @@
 set -ex
+TARGETARCH=amd64
 hubhost=ccr.ccs.tencentyun.com/cube-studio
 
 base_image=nvidia/cuda:11.8.0-cudnn8-runtime-ubuntu20.04
-TARGETARCH=amd64
+
 docker build -t $hubhost/ubuntu-gpu:cuda11.8.0-cudnn8-$TARGETARCH --build-arg FROM_IMAGES=$base_image -f cuda/Dockerfile .
-docker build -t $hubhost/ubuntu-gpu:cuda11.8.0-cudnn8-python3.7-$TARGETARCH --build-arg FROM_IMAGES=$hubhost/ubuntu-gpu:cuda11.8.0-cudnn8-$TARGETARCH --build-arg PYTHON_VERSION=python3.7 -f cuda/python/Dockerfile .
-docker build -t $hubhost/ubuntu-gpu:cuda11.8.0-cudnn8-python3.8-$TARGETARCH --build-arg FROM_IMAGES=$hubhost/ubuntu-gpu:cuda11.8.0-cudnn8-$TARGETARCH --build-arg PYTHON_VERSION=python3.8 -f cuda/python/Dockerfile .
 docker build -t $hubhost/ubuntu-gpu:cuda11.8.0-cudnn8-python3.9-$TARGETARCH --build-arg FROM_IMAGES=$hubhost/ubuntu-gpu:cuda11.8.0-cudnn8-$TARGETARCH --build-arg PYTHON_VERSION=python3.9 -f cuda/python/Dockerfile .
 
 docker push $hubhost/ubuntu-gpu:cuda11.8.0-cudnn8-$TARGETARCH
-docker push $hubhost/ubuntu-gpu:cuda11.8.0-cudnn8-python3.7-$TARGETARCH
-docker push $hubhost/ubuntu-gpu:cuda11.8.0-cudnn8-python3.8-$TARGETARCH
 docker push $hubhost/ubuntu-gpu:cuda11.8.0-cudnn8-python3.9-$TARGETARCH
+
+# docker manifest create $hubhost/ubuntu-gpu:cuda11.8.0-cudnn8-python3.9 $hubhost/ubuntu-gpu:cuda11.8.0-cudnn8-python3.9-amd64 $hubhost/ubuntu-gpu:cuda11.8.0-cudnn8-python3.9-arm64 && docker manifest push $hubhost/ubuntu-gpu:cuda11.8.0-cudnn8-python3.9
+
+base_image=nvidia/cuda:12.1.0-cudnn8-runtime-ubuntu20.04
+
+docker build -t $hubhost/ubuntu-gpu:cuda12.1.0-cudnn8-$TARGETARCH --build-arg FROM_IMAGES=$base_image -f cuda/Dockerfile .
+docker build -t $hubhost/ubuntu-gpu:cuda12.1.0-cudnn8-python3.9-$TARGETARCH --build-arg FROM_IMAGES=$hubhost/ubuntu-gpu:cuda12.2.2-cudnn8-$TARGETARCH --build-arg PYTHON_VERSION=python3.9 -f cuda/python/Dockerfile .
+
+docker push $hubhost/ubuntu-gpu:cuda12.1.0-cudnn8-$TARGETARCH
+docker push $hubhost/ubuntu-gpu:cuda12.1.0-cudnn8-python3.9-$TARGETARCH
+
+
