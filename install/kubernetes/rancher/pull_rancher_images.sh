e2f3946bcb1bc4b9a0d9b80655307014c477c21a
data-infra <825485697@qq.com>
2024-06-30 21:58:31 +0800

    更换docker镜像源

--- install/kubernetes/rancher/pull_rancher_images.sh
+++ install/kubernetes/rancher/pull_rancher_images.sh
@@ -2,6 +2,7 @@
 docker pull ccr.ccs.tencentyun.com/cube-rancher/rancher-webhook:v0.2.1 && docker tag ccr.ccs.tencentyun.com/cube-rancher/rancher-webhook:v0.2.1 rancher/rancher-webhook:v0.2.1 &
 docker pull ccr.ccs.tencentyun.com/cube-rancher/rancher:v2.6.2 && docker tag ccr.ccs.tencentyun.com/cube-rancher/rancher:v2.6.2 rancher/rancher:v2.6.2 &
 docker pull ccr.ccs.tencentyun.com/cube-rancher/mirrored-pause:3.4.1 && docker tag ccr.ccs.tencentyun.com/cube-rancher/mirrored-pause:3.4.1 rancher/mirrored-pause:3.4.1 &
+docker pull ccr.ccs.tencentyun.com/cube-rancher/mirrored-pause:3.6 && docker tag ccr.ccs.tencentyun.com/cube-rancher/mirrored-pause:3.6 rancher/mirrored-pause:3.6 &
 docker pull ccr.ccs.tencentyun.com/cube-rancher/mirrored-calico-pod2daemon-flexvol:v3.19.2 && docker tag ccr.ccs.tencentyun.com/cube-rancher/mirrored-calico-pod2daemon-flexvol:v3.19.2 rancher/mirrored-calico-pod2daemon-flexvol:v3.19.2 &
 docker pull ccr.ccs.tencentyun.com/cube-rancher/rke-tools:v0.1.78 && docker tag ccr.ccs.tencentyun.com/cube-rancher/rke-tools:v0.1.78 rancher/rke-tools:v0.1.78 &
 docker pull ccr.ccs.tencentyun.com/cube-rancher/rke-tools:v0.1.87 && docker tag ccr.ccs.tencentyun.com/cube-rancher/rke-tools:v0.1.87 rancher/rke-tools:v0.1.87 &
@@ -17,6 +18,7 @@ docker pull ccr.ccs.tencentyun.com/cube-rancher/mirrored-metrics-server:v0.5.0 &
 docker pull ccr.ccs.tencentyun.com/cube-rancher/rancher-runtime:v2.6.2 && docker tag ccr.ccs.tencentyun.com/cube-rancher/rancher-runtime:v2.6.2 rancher/rancher-runtime:v2.6.2 &
 docker pull ccr.ccs.tencentyun.com/cube-rancher/mirrored-coreos-etcd:v3.4.16-rancher1 && docker tag ccr.ccs.tencentyun.com/cube-rancher/mirrored-coreos-etcd:v3.4.16-rancher1 rancher/mirrored-coreos-etcd:v3.4.16-rancher1 &
 docker pull ccr.ccs.tencentyun.com/cube-rancher/mirrored-coreos-flannel:v0.14.0 && docker tag ccr.ccs.tencentyun.com/cube-rancher/mirrored-coreos-flannel:v0.14.0 rancher/mirrored-coreos-flannel:v0.14.0 &
+docker pull ccr.ccs.tencentyun.com/cube-rancher/mirrored-coreos-flannel:v0.15.1 && docker tag ccr.ccs.tencentyun.com/cube-rancher/mirrored-coreos-flannel:v0.15.1 rancher/mirrored-coreos-flannel:v0.15.1 &
 docker pull ccr.ccs.tencentyun.com/cube-rancher/mirrored-calico-kube-controllers:v3.19.2 && docker tag ccr.ccs.tencentyun.com/cube-rancher/mirrored-calico-kube-controllers:v3.19.2 rancher/mirrored-calico-kube-controllers:v3.19.2 &
 docker pull ccr.ccs.tencentyun.com/cube-rancher/mirrored-calico-node:v3.19.2 && docker tag ccr.ccs.tencentyun.com/cube-rancher/mirrored-calico-node:v3.19.2 rancher/mirrored-calico-node:v3.19.2 &
 docker pull ccr.ccs.tencentyun.com/cube-rancher/mirrored-coredns-coredns:1.8.4 && docker tag ccr.ccs.tencentyun.com/cube-rancher/mirrored-coredns-coredns:1.8.4 rancher/mirrored-coredns-coredns:1.8.4 &
